4b34aefd65c9e8bc8f2507107979583382ba08e7
Felix Freidman <felixfreidman@gmail.com>
2022-04-07 22:02:16 +0800

    10:01PM Commit Desktop is Done Mobile is in Progress

--- src/assets/js/components/common.js
+++ src/assets/js/components/common.js
@@ -7,4 +7,41 @@ if (!windowLocation.includes("privacy")) {
       question.classList.toggle("faq-block--active");
     });
   });
+
+  const darkLayer = document.querySelector(".dark-layer");
+  const applyBtns = document.querySelectorAll(".apply-btn");
+  const closeForm = document.getElementById("closeForm");
+  const applyFormScreen = document.getElementById("applyForm");
+  const burgerMenu = document.querySelector(".mobile-header__burger-menu");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const mobileNavigation = document.getElementById("mobileNavigation");
+  const allLinks = mobileNavigation.querySelectorAll(".navigation-link");
+
+  applyBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+      darkLayer.classList.toggle("js--hidden");
+    });
+  });
+
+  closeForm.addEventListener("click", () => {
+    darkLayer.classList.toggle("js--hidden");
+  });
+
+  burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("mobile-header__burger-menu--active");
+    mobileMenu.classList.toggle("mobile-menu--active");
+    document.querySelector("body").classList.toggle("disable-body");
+  });
+
+  allLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      burgerMenu.classList.toggle("mobile-header__burger-menu--active");
+      mobileMenu.classList.toggle("mobile-menu--active");
+      document.querySelector("body").classList.toggle("disable-body");
+    });
+  });
+
+  $(document).ready(function () {
+    $("#userPhone").inputmask();
+  });
 }
