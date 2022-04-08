bc8669aa44e0ba372ff4d6dcdea56b19848e8626
Felix Freidman <felixfreidman@gmail.com>
2022-04-08 16:53:19 +0800

    4:52PM Commit Сделал верстку, переношу на CMS

---
+++ src/php/functions.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Tadam Theme functions and definitions
+ *
+ */
+function Tadam_theme_support()
+{
+    wp_enqueue_style("style.min.css", 'all');
+    add_theme_support('post-thumbnails');
+    set_post_thumbnail_size(1200, 9999);
+    add_image_size('twentytwenty-fullscreen', 1980, 9999);
+    add_theme_support('title-tag');
+}
+
+add_action("after_setup_theme", "Tadam_theme_support");
+
+// Настройка для админки
+
+if (function_exists('acf_add_options_page')) {
+
+    acf_add_options_page(array(
+        'page_title' => 'Основные настройки',
+        'menu_title' => 'Настройки сайта',
+        'menu_slug' => 'theme-general-settings',
+        'capability' => 'edit_posts',
+        'redirect' => false,
+    ));
+}
+
+// Register Styles and Scripts
+
+function Tadam_register_styles()
+{
+
+    wp_enqueue_style('main-style-css', get_stylesheet_uri() . "/main.min.css");
+}
+
+add_action('wp_enqueue_scripts', 'Tadam_register_styles');
+
+function Tadam_register_scripts()
+{
+    wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.min.js');
+}
+
+add_action('wp_enqueue_scripts', 'Tadam_register_scripts');
+
+// AJAX и отправка писем
+
+function ajax_form_scripts()
+{
+    $translation_array = array(
+        'ajax_url' => admin_url('admin-ajax.php'),
+    );
+    wp_localize_script('main', 'cpm_object', $translation_array);
+}
+
+add_action('wp_enqueue_scripts', 'ajax_form_scripts');
+function ajax_form()
+{
+    $name = $_REQUEST['name'];
+    $phone = $_REQUEST['phone'];
+    $company = $_REQUEST['company'];
+    $area = $_REQUEST['area'];
+    $response = '';
+    $thm = 'Заявка на демо-доступ';
+    $thm = "=?utf-8?b?" . base64_encode($thm) . "?=";
+    $msg = "Имя: " . $name . "<br />
+    Телефон: " . $phone . "<br />
+    Название компании: " . $company . "<br />
+    Область деятельности: " . $area . "<br />";
+    $mail_to = get_field("email_to", 'option');
+
+    $headers = "Content-Type: text/html; charset=utf-8\n";
+    $headers .= 'От: Tadam.Посадка' . "\r\n";
+
+// Отправляем почтовое сообщение
+
+    if (mail($mail_to, $thm, $msg, $headers)) {
+        $response = '<div class="applied-screen" id="appliedForm">
+<div class="form-close" id="closeAppliedForm"><span class="cross-one"> </span><span class="cross-two"></span>
+</div><img class="applied-icon"
+    src="http://localhost:8888/wp-content/themes/Tadam/assets/images/content/main__applied.svg"
+alt="Form Applied">
+<div class="applied-header">Спасибо за заявку</div>
+<div class="applied-subheader">Менеджер свяжется с вами в ближайшее время</div>
+</div>';
+    } else {
+        $response = 'Ошибка при отправке';
+    }
+
+// Сообщение о результате отправки почты
+
+    if (defined('DOING_AJAX') && DOING_AJAX) {
+        echo $response;
+        wp_die();
+    }
+}
+
+add_action('wp_ajax_nopriv_ajax_form', 'ajax_form');
+add_action('wp_ajax_ajax_form', 'ajax_form');
---
+++ src/php/page_main.php
@@ -0,0 +1,279 @@
+<?php /* Template Name: Main Page */?>
+<?php get_header()?>
+<main class="main">
+    <section class="section main--greeting">
+        <div class="blue-circle blue-circle--left"> </div>
+        <div class="blue-circle blue-circle--right"> </div>
+        <div class="section-container">
+            <div class="greeting-container">
+                <div class="greeting-header">Реклама вашего мероприятия</div>
+                <div class="greeting-subheader"> Управляйте рекламой без рутины и лишних расходов<span> Уже </span><a
+                        class="greeting-subheader__link" href="#">более 1 000 активных пользователей</a> в Москве</div>
+                <div class="greeting-button apply-btn">Попробовать</div>
+            </div>
+            <div class="greeting-image"> <img src="./assets/images/content/main__greeting.svg" alt="Main Greeting">
+            </div>
+        </div>
+        <div class="stats-container">
+            <div class="stats-container__header">Исходя из статистики рекламодателей Tadam</div>
+            <div class="stats-container__cards">
+                <div class="stats-container__card">
+                    <div class="card-image"> <img src="./assets/images/content/main__stats1.svg" alt=""></div>
+                    <div class="card-header">520 ₽ </div>
+                    <div class="card-content">Средняя стоимость привлечения клиента</div>
+                </div>
+                <div class="stats-container__card">
+                    <div class="card-image"> <img src="./assets/images/content/main__stats2.svg" alt=""></div>
+                    <div class="card-header">6 часов</div>
+                    <div class="card-content">До получения первых клиентов</div>
+                </div>
+                <div class="stats-container__card">
+                    <div class="card-image"> <img src="./assets/images/content/main__stats3.svg" alt=""></div>
+                    <div class="card-header">до 1 000 новых клиентов </div>
+                    <div class="card-content">На мероприятие любой тематики</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section main--advert" id="toAbout">
+        <div class="adverter-container">
+            <div class="content">
+                <div class="adverter-container__header">Tadam — социальная сеть о развлечениях</div>
+                <div class="adverter-container__subheader">Мы — развлекательный навигатор нового поколения, который
+                    первым оперативно сообщает и продвигает события и мероприятия, которые нельзя пропустить</div>
+                <div class="adverter-container__download">
+                    <a class="download-link download-link--google" href="#"> </a>
+                    <a class="download-link download-link--apple" href="#"></a>
+                </div>
+            </div>
+            <div class="adverter-container__image"> <img src="./assets/images/content/main_advert__phone.svg"
+                    alt="Advert Image"></div>
+        </div>
+    </section>
+    <section class="section main--ability" id="toAbilities">
+        <div class="section-header">Tadam поможет</div>
+        <div class="ability-container">
+            <div class="ability-container__card">
+                <div class="card-image"><img src="./assets/images/content/main__ability1.svg" alt="Ability 1"></div>
+                <div class="card-header">Управлять трафиком</div>
+                <div class="card-content">В нашем приложении тысячи пользователей, которые постоянно ищут интересные
+                    мероприятия и локации и хотят разнообразить свой досуг</div>
+            </div>
+            <div class="ability-container__card">
+                <div class="card-image"><img src="./assets/images/content/main__ability2.svg" alt="Ability 2"></div>
+                <div class="card-header">Работать с узнаваемостью </div>
+                <div class="card-content">Весь город узнает о вашем заведении или событии, сотни людей захотят его
+                    посетить</div>
+            </div>
+            <div class="ability-container__card">
+                <div class="card-image"><img src="./assets/images/content/main__ability3.svg" alt="Ability 3"></div>
+                <div class="card-header">С пиаром Вашего событиям</div>
+                <div class="card-content">Чем больше людей узнает о вашем событии, тем больше своих друзей и знакомых
+                    они пригласят!</div>
+            </div>
+        </div>
+        <div class="ability-container">
+            <div class="ability-container__card">
+                <div class="card-image"><img src="./assets/images/content/main__ability4.svg" alt="Ability 4"></div>
+                <div class="card-header">Собирать контакты и заявки</div>
+                <div class="card-content">Максимально удобно собирать заявки в одном сервисе. А вашим пользователям тоже
+                    будет удобно пройти простую понятную регистрацию.</div>
+            </div>
+            <div class="ability-container__card">
+                <div class="card-image"><img src="./assets/images/content/main__ability5.svg" alt="Ability 5"></div>
+                <div class="card-header">Собирать обратную связь</div>
+                <div class="card-content">В Tadam вам будут доступны контакты людей, которые зарегистрировались на ваше
+                    событие. А также чат события, где будут видны честные комментарии участников.</div>
+            </div>
+            <div class="ability-container__card positionedAbs">
+                <div class="card-image"><img src="./assets/images/content/main__ability6.svg" alt="Ability 6"></div>
+                <div class="card-header">Сформировать лояльность</div>
+                <div class="card-content">Аудитория доверяет проверенным организаторам. В Tadam пользователям будет
+                    видно историю ваших событий и отзывы о прошедших мероприятий.</div>
+            </div>
+        </div>
+    </section>
+    <section class="section main--everybody" id="toProcess">
+        <div class="section-header">Рекламу может настроить каждый</div>
+        <div class="everybody-container everybody-container--blue">
+            <div class="everybody-container__box">
+                <div class="everybody-container__counter">01</div>
+                <div class="everybody-container__header">Зайдите в рекламный кабинет с помощью компьютера или телефона
+                </div>
+                <div class="everybody-container__content">
+                    <div class="everybody-container__subheader">Простая авторизация с помощью номера телефона</div>
+                    <div class="everybody-container__paragraph">Если у вас есть аккаунт в приложении Tadam, то не нужно
+                        создавать новый, просто зайдите с помощью предыдущих настроек</div>
+                </div>
+            </div>
+            <div class="everybody-container__image"> <img src="./assets/images/content/main__everybody1.svg"
+                    alt="Main Evetybody 1"></div>
+        </div>
+        <div class="everybody-container everybody-container--yellow">
+            <div class="everybody-container__box">
+                <div class="everybody-container__counter">02</div>
+                <div class="everybody-container__header">Заполните данные о себе или вашей компании</div>
+                <div class="everybody-container__content">
+                    <div class="everybody-container__subheader">Зарегистрируйтесь как физическое лицо или компания</div>
+                    <div class="everybody-container__paragraph">Расскажите о себе или вашей компании<br>Дайте доступ
+                        вашим сотрудникам для совместного пользования рекламным кабентомновый, просто зайдите с помощью
+                        предыдущих настроек</div>
+                </div>
+            </div>
+            <div class="everybody-container__image"> <img src="./assets/images/content/main__everybody2.svg"
+                    alt="Main Evetybody 2"></div>
+        </div>
+        <div class="everybody-container everybody-container--green">
+            <div class="everybody-container__box">
+                <div class="everybody-container__counter">03</div>
+                <div class="everybody-container__header">Создайте карточку события и запустите рекламную кампанию</div>
+                <div class="everybody-container__content">
+                    <div class="everybody-container__subheader">Прикрепите событие к рекламной кампании и выберете
+                        настройки показа:</div>
+                    <ul class="everybody-container__list">
+                        <li class="everybody-container__list-item">Пол</li>
+                        <li class="everybody-container__list-item">Возраст</li>
+                        <li class="everybody-container__list-item">Интересы</li>
+                        <li class="everybody-container__list-item">Географию показа </li>
+                        <li class="everybody-container__list-item">и пр.</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="everybody-container__image"> <img src="./assets/images/content/main__everybody3.svg"
+                    alt="Main Evetybody 3"></div>
+        </div>
+    </section>
+    <section class="section main--level">
+        <div class="section-header">Реклама для бизнеса любого уровня</div>
+        <div class="level-container">
+            <div class="level-container__card">
+                <div class="card-image" style="background: url(./assets/images/content/main__level1.svg)" alt="Level 1">
+                </div>
+                <div class="card-content">
+                    <div class="card-content__header">Для небольших вечеринок</div>
+                    <div class="card-content__content">Организуйте крутую вечеринку, пригласив на нее своих будущих
+                        новых друзей</div>
+                </div>
+            </div>
+            <div class="level-container__card">
+                <div class="card-image" style="background: url(./assets/images/content/main__level2.svg)" alt="Level 2">
+                </div>
+                <div class="card-content">
+                    <div class="card-content__header">Для рок-группы и камерного оркестра</div>
+                    <div class="card-content__content">Проведите музыкальный концерт своей группы, собрав отзывчивую
+                        аудиторию</div>
+                </div>
+            </div>
+            <div class="level-container__card">
+                <div class="card-image" style="background: url(./assets/images/content/main__level3.svg)" alt="Level 3">
+                </div>
+                <div class="card-content">
+                    <div class="card-content__header">Для профессиональных агентств организации мероприятий</div>
+                    <div class="card-content__content">Соберите крупное мероприятие более чем на 250 человек</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section main--now">
+        <div class="now-header">Начните рекламировать свое мероприятие прямо сейчас!</div>
+        <div class="now-subheader">Заполните форму и получите демо-доступ к рекламному кабинету Tadam</div>
+        <div class="now-container">
+            <div class="now-container__button apply-btn">Попробовать</div>
+            <div class="now-container__image"> <img src="./assets/images/content/main__now.svg" alt="Now 1"></div>
+        </div>
+    </section>
+    <section class="section main--help" id="toHelp">
+        <div class="section-header">Часто задаваемые вопросы</div>
+        <div class="help-container">
+            <div class="faq-container">
+                <div class="faq-block">
+                    <div class="faq-question">
+                        <div class="faq-question__content">Окей, а кто сидит в Tadam</div>
+                        <div class="question-indicator"> <span class="cross-one"> </span><span class="cross-two"></span>
+                        </div>
+                    </div>
+                    <div class="faq-answer">
+                        <div class="faq-answer__content">Наша аудитория — это молодая и активная часть Москвы! Это парни
+                            и девушки от 18 в среднем до 35 лет. Они привыкли посещать новые места и открывать для себя
+                            интересные события, охотно рассказывать об этом друзьям и делиться
+                            впечатлениями. В тоже время они всегда рады вернуться в любимое заведение, чтобы насладиться
+                            знакомой и приятной атмосферой.</div><a class="faq-answer__link" href="#">Подробнее об
+                            аудитории</a>
+                    </div>
+                </div>
+                <div class="faq-block">
+                    <div class="faq-question">
+                        <div class="faq-question__content">Сколько человек зарегистрируется на мое мероприятие?</div>
+                        <div class="question-indicator"> <span class="cross-one"> </span><span class="cross-two"></span>
+                        </div>
+                    </div>
+                    <div class="faq-answer">
+                        <div class="faq-answer__content">Наша аудитория — это молодая и активная часть Москвы! Это парни
+                            и девушки от 18 в среднем до 35 лет. Они привыкли посещать новые места и открывать для себя
+                            интересные события, охотно рассказывать об этом друзьям и делиться
+                            впечатлениями. В тоже время они всегда рады вернуться в любимое заведение, чтобы насладиться
+                            знакомой и приятной атмосферой.</div><a class="faq-answer__link" href="#">Подробнее об
+                            аудитории</a>
+                    </div>
+                </div>
+                <div class="faq-block">
+                    <div class="faq-question">
+                        <div class="faq-question__content">Что нельзя публиковать?</div>
+                        <div class="question-indicator"> <span class="cross-one"> </span><span class="cross-two"></span>
+                        </div>
+                    </div>
+                    <div class="faq-answer">
+                        <div class="faq-answer__content">Наша аудитория — это молодая и активная часть Москвы! Это парни
+                            и девушки от 18 в среднем до 35 лет. Они привыкли посещать новые места и открывать для себя
+                            интересные события, охотно рассказывать об этом друзьям и делиться
+                            впечатлениями. В тоже время они всегда рады вернуться в любимое заведение, чтобы насладиться
+                            знакомой и приятной атмосферой.</div><a class="faq-answer__link" href="#">Подробнее об
+                            аудитории</a>
+                    </div>
+                </div>
+                <div class="faq-block">
+                    <div class="faq-question">
+                        <div class="faq-question__content">Как аудитория находит Tadam?</div>
+                        <div class="question-indicator"> <span class="cross-one"> </span><span class="cross-two"></span>
+                        </div>
+                    </div>
+                    <div class="faq-answer">
+                        <div class="faq-answer__content">Наша аудитория — это молодая и активная часть Москвы! Это парни
+                            и девушки от 18 в среднем до 35 лет. Они привыкли посещать новые места и открывать для себя
+                            интересные события, охотно рассказывать об этом друзьям и делиться
+                            впечатлениями. В тоже время они всегда рады вернуться в любимое заведение, чтобы насладиться
+                            знакомой и приятной атмосферой.</div><a class="faq-answer__link" href="#">Подробнее об
+                            аудитории</a>
+                    </div>
+                </div>
+                <div class="faq-block">
+                    <div class="faq-question">
+                        <div class="faq-question__content">Вижу ошибки в рекламном кабинете. Что делать?</div>
+                        <div class="question-indicator"> <span class="cross-one"> </span><span class="cross-two"></span>
+                        </div>
+                    </div>
+                    <div class="faq-answer">
+                        <div class="faq-answer__content">Наша аудитория — это молодая и активная часть Москвы! Это парни
+                            и девушки от 18 в среднем до 35 лет. Они привыкли посещать новые места и открывать для себя
+                            интересные события, охотно рассказывать об этом друзьям и делиться
+                            впечатлениями. В тоже время они всегда рады вернуться в любимое заведение, чтобы насладиться
+                            знакомой и приятной атмосферой.</div><a class="faq-answer__link" href="#">Подробнее об
+                            аудитории</a>
+                    </div>
+                </div>
+            </div>
+            <div class="faq-questionary">
+                <div class="blue-circle"> </div>
+                <div class="faq-questionary__image"> <img src="./assets/images/content/main__question.svg"
+                        alt="Question Logo"></div>
+                <div class="faq-questionary__header">Остались вопросы?</div>
+                <div class="faq-questionary__subheader">Оставьте заявку и наш менеджер свяжется с вами чтобы ответить на
+                    все вопросы</div>
+                <div class="faq-questionary__button apply-btn">Оставить заявку </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+
+<?php get_footer()?>
